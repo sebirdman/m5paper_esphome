f2126f2fd213ef4d5b6c7f250f11c3f49207fa59
Pavlo Dudnytskyi <paveldudn@gmail.com>
2023-07-26 17:08:27 +0200

    Fixed screen for 2023.7.0

--- custom_components/it8951e/display.py
+++ custom_components/it8951e/display.py
@@ -64,7 +64,7 @@ async def to_code(config):
         cg.add(var.set_cs_pin(cs))
     if CONF_LAMBDA in config:
         lambda_ = await cg.process_lambda(
-            config[CONF_LAMBDA], [(display.DisplayBufferRef, "it")], return_type=cg.void
+            config[CONF_LAMBDA], [(display.DisplayRef, "it")], return_type=cg.void
         )
         cg.add(var.set_writer(lambda_))
     if CONF_RESET_PIN in config:
